ca5d6d8e09ec334a88db5fb54d2890b02787395f
ragnar170 <36407407+AnuragP170@users.noreply.github.com>
2023-11-30 23:04:44 +0800

    Update packet_handler.c

--- Attempted_Final_Integration/src/packet_handler.c
+++ Attempted_Final_Integration/src/packet_handler.c
@@ -8,12 +8,15 @@
 #include "lwip/pbuf.h"
 #include "../no-OS-FatFS-SD-SPI-RPi-Pico-master/FatFs_SPI/ff14a/source/ff.h"
 
+// Define individual TCP flags
 #define FIN_FLAG 0b00000001
 #define SYN_FLAG 0b00000010
 #define RST_FLAG 0b00000100
 #define PSH_FLAG 0b00001000
 #define ACK_FLAG 0b00010000
 #define URG_FLAG 0b00100000
+
+// Define combinations of TCP flags for specific scenarios
 #define SYN_RST_FLAG (SYN_FLAG | RST_FLAG)
 #define SYN_ACK_FLAG (SYN_FLAG | ACK_FLAG)
 #define RST_ACK_FLAG (RST_FLAG | ACK_FLAG)
@@ -39,7 +42,9 @@ struct FlagMap
     const char *name;
 };
 
-// Mapping of flags to their values and names
+
+// Structure defining a mapping of TCP flags represented in binary to their corresponding names.
+// Each entry in the flag_map[] array pairs a binary flag value with its symbolic name.
 struct FlagMap flag_map[] = {
     {FIN_FLAG, "FIN"},
     {SYN_FLAG, "SYN"},
@@ -54,7 +59,7 @@ struct FlagMap flag_map[] = {
     {PSH_ACK_FLAG, "PSH_ACK"},
     {FIN_PSH_URG_FLAG, "FIN_PSH_URG"}};
 
-// structure to hold ntp connection
+// structure to hold ntp connection state
 typedef struct NTP_T_
 {
     ip_addr_t ntp_server_address[4];
@@ -62,14 +67,26 @@ typedef struct NTP_T_
     bool ntp_get;
 } NTP_T;
 
-// to increment time
+/**
+ * Increments the epoch time by one second.
+ * 
+ * @return Returns true upon successful increment of the epoch time.
+ */
 bool increment_epoch()
 {
     epoch_time++;
     return true;
 }
 
-// NTP data received
+/**
+ function invoked upon receiving NTP data over UDP.
+ * 
+ * @param arg       Pointer to the NTP state structure.
+ * @param pcb       Pointer to the UDP protocol control block.
+ * @param p         Pointer to the received packet buffer.
+ * @param addr      Pointer to the IP address of the sender.
+ * @param port      Port number from which the packet was received.
+ */
 static void ntp_recv(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_addr_t *addr, u16_t port)
 {
     NTP_T *state = (NTP_T *)arg;
@@ -91,7 +108,11 @@ static void ntp_recv(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_ad
     pbuf_free(p);
 }
 
-// get epoch value from ntp server
+/**
+ * Obtains the epoch time value from NTP servers.
+ * 
+ * @return Returns 0 upon successful retrieval of epoch time; otherwise, returns 1 in case of failure.
+ */
 int get_epoch()
 {
     if (cyw43_arch_init())
@@ -139,20 +160,43 @@ int get_epoch()
     return 0;
 }
 
-// Function to check if packet is TCP with specific flag
+/**
+ * Checks if the packet in the buffer at the specified position represents an IPv4 packet with TCP protocol.
+ * 
+ * @param buffer    Pointer to the start of the packet buffer.
+ * @param position  Position within the buffer where the function starts checking for the headers.
+ * @return          Returns 1 if the packet at the given position is an IPv4 packet with TCP protocol, else returns 0.
+ */
 int is_ipv4_tcp(unsigned char *buffer, size_t position)
 {
     return (buffer[position] == 0x08 && buffer[position + 1] == 0x00 &&
             buffer[position + 11] == 0x06);
 }
 
+/**
+ * Checks if a specific flag in the TCP header of an IPv4 packet is set to a given value.
+ * 
+ * @param buffer         Pointer to the start of the packet buffer.
+ * @param position       Position within the buffer where the function starts checking for the headers.
+ * @param flag_to_check  The value of the flag to be checked in the TCP header.
+ * @return               Returns 1 if the specified flag in the TCP header matches the given value, else returns 0.
+ */
 int check_flag(unsigned char *buffer, size_t position, int flag_to_check)
 {
     return (buffer[position] == 0x08 && buffer[position + 1] == 0x00 &&
             buffer[position + 11] == 0x06 && buffer[position + 35] == flag_to_check);
 }
 
-// Convert selected packet fields into ASCII
+
+
+/**
+ * Parses a network packet stored in the buffer to extract specific information, primarily focusing on flags within the packet headers (Convert selected packet fields into ASCII).
+ * 
+ * @param buffer            Pointer to the start of the packet buffer.
+ * @param size              Size of the packet buffer.
+ * @param currentPosition   Current position within the buffer to begin parsing.
+ * @return                  Returns a string containing parsed information from the packet, or NULL if no matching flag is found.
+ */
 char *parse_packet(unsigned char *buffer, size_t size, size_t currentPosition)
 {
     int i;
@@ -160,12 +204,16 @@ char *parse_packet(unsigned char *buffer, size_t size, size_t currentPosition)
     time_t time = (time_t)epoch_time;
     struct tm *utc = gmtime(&time);
 
+    // Iterate through packet buffer
     for (i = currentPosition; i < size - 13; ++i)
     {
+         // Check each flag in the flag_map array for a match in the packet
         for (int j = 0; j < sizeof(flag_map) / sizeof(flag_map[0]); ++j)
         {
+           
             if (check_flag(buffer, i, flag_map[j].value))
             {
+                // Extract information if flag is found into string
                 snprintf(output, 128, "%02d/%02d/%04d %02d:%02d:%02d,%d.%d.%d.%d,%d,%d.%d.%d.%d,%d,%s\n", utc->tm_mday, utc->tm_mon + 1, utc->tm_year + 1900,
                          utc->tm_hour, utc->tm_min, utc->tm_sec,
                          buffer[i + 14], buffer[i + 15], buffer[i + 16], buffer[i + 17],
@@ -182,19 +230,34 @@ char *parse_packet(unsigned char *buffer, size_t size, size_t currentPosition)
     return NULL;
 }
 
+/**
+ * Searches for the next potential start position of a network packet within the buffer.
+ * 
+ * @param buffer        Pointer to the start of the packet buffer.
+ * @param bufferSize    Size of the packet buffer.
+ * @param startFrom     Position within the buffer to start searching for the next packet.
+ * @return              Returns the position of the next potential packet start within the buffer, or bufferSize if no more packets are found.
+ */
 size_t find_next_packet(unsigned char *buffer, size_t bufferSize, size_t startFrom)
 {
-    for (size_t i = startFrom + 1; i < bufferSize - 1; ++i) // start searching from startFrom + 1
+    // Start searching for the next potential packet start position from startFrom + 1
+    for (size_t i = startFrom + 1; i < bufferSize - 1; ++i)
     {
+        // Check if the current position indicates the start of an TCP/IP packet
         if (is_ipv4_tcp(buffer, i))
         {
+            // return position of next packet
             return i;
         }
     }
     return bufferSize; // Indicates no more packets found
 }
 
-// Function to detect SYN packet
+/**
+ * Detects SYN packets and counts potential SYN flood occurrences.
+ * 
+ * @param packet Pointer to the network packet data to analyze.
+ */
 void detect_syn_flood(unsigned char *packet)
 {
     if ((strstr(packet, "SYN") != NULL) && (strstr(packet,"SYN_RST")==NULL)&& (strstr(packet,"SYN_ACK")==NULL))
@@ -208,23 +271,30 @@ void detect_syn_flood(unsigned char *packet)
         char dest_ip[16];
         int dest_port;
         char flag[4];
+        
+        // Extract details from the packet
         sscanf(packet, "%19[^,],%15[^,],%d,%15[^,],%d,%3s", &current_time, &src_ip, src_port, &dest_ip, dest_port, flag);
+
+        // Check for synchronization or change in destination IP
         if ((sync_counter == 0) || (strcmp(first_time, current_time) != 0))
         {
+            // Update first-time stamp and destination IP when conditions are met
             strcpy(first_time, current_time);
             strcpy(first_dest_ip, dest_ip);
             sync_counter = 0;
         }
+        
+        // Increment sync_counter if the destination IP matches the stored first destination IP
         if (strcmp(first_dest_ip, dest_ip) == 0)
         {
             sync_counter++;
         }
-        // uncomment to see information
-         printf("Time: %d\n", time);
-         printf("Source IP: %s\n", src_ip);
-         printf("Source Port: %d\n", src_port);
-         printf("Destination IP: %s\n", dest_ip);
-         printf("Destination Port: %d\n", dest_port);
-         printf("Flag: %s\n", flag);
+        // uncomment to see information for debugging only
+         // printf("Time: %d\n", time);
+         // printf("Source IP: %s\n", src_ip);
+         // printf("Source Port: %d\n", src_port);
+         // printf("Destination IP: %s\n", dest_ip);
+         // printf("Destination Port: %d\n", dest_port);
+         // printf("Flag: %s\n", flag);
     }
 }
